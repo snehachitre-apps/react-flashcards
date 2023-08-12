fca00f1dca92ab1fca16c413010d72609ee74cdc
snehachitre-apps <sneha.chitre.usa@gmail.com>
2023-08-11 22:07:44 -0500

    basic flash card app skeleton

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { useState } from 'react';
+
+
+function Card({frontSide,backSide}){
+
+  const [isFront,changeFace] = useState(true);
+
+    function handleClick(){
+        //we are doing this so that we can click back to the front side
+      changeFace(oldState => !oldState);
+    }
+    const text = isFront ? frontSide : backSide;
+    const sideClass = isFront ? "front" : "back";
+    const classList = `flash-card ${sideClass}`;
+
+  return <div className={classList} onClick={handleClick}>{text}</div>;
+
+}
+export default Card;
