50764ebb43d508ae74f9d65fa222f4d7049af3ab
snehachitre-apps <sneha.chitre.usa@gmail.com>
2023-09-04 16:15:24 -0500

    flashcards app

--- src/components/ShowQuestions.js
+++ src/components/ShowQuestions.js
@@ -7,32 +7,37 @@ export default function ShowQuestions({questions})	 {
 
 let randomQ={};
 let questionsCopy = questions;
-   
+ 
 const [next, setNext] = useState(false);
-
+const [state,setState] = useState();
+let counter=0;
 
 const handleNext =() => {
 
     setNext(true);
     randomQ = Math.floor(Math.random()*questionsCopy.length);
-    console.log("next", next);
+
     
     console.log("new question",questionsCopy[randomQ]);
-   
-
-  
-       
+    setState( <Card frontSide={questionsCopy[randomQ].question} backSide ={questionsCopy[randomQ].correct_answer} />);
+    
 }   
-
-
-
+//first question
 randomQ = Math.floor(Math.random()*questionsCopy.length);  
+counter++;
+
     return (
         
         <div className="question-card">
-            <Card frontSide={questionsCopy[randomQ].question} backSide ={questionsCopy[randomQ].correct_answer} />
+             
+            {counter==1 ? <Card frontSide={questionsCopy[randomQ].question.replace(/(&quot\;)/g,"\"").replace(/(&#039\;)/g,"\'")} backSide ={questionsCopy[randomQ].correct_answer} />:null}
+
+          {/* figure out a way  to replace all special characters code wth the characters*/}
+         
             <button className="next" onClick={handleNext}>Next</button>
-            {next ?  getActiveElement.bind(<Card frontSide={questionsCopy[randomQ].question} backSide ={questionsCopy[randomQ].correct_answer} />) : null}
+            {next && counter>1?   <Card frontSide={questionsCopy[randomQ].question.replace(/(&quot\;)/g,"\"").replace(/(&#039\;)/g,"\'")} backSide ={questionsCopy[randomQ].correct_answer} />:null} 
+         
+           
         </div>
     )
     
